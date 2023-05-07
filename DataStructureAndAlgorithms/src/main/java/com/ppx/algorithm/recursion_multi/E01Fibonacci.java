d33febdaefbac85a2008d2fa5f6288fd4a39ec1a
wuyi <956396908@qq.com>
2023-05-08 00:05:32 +0800

    5

---
+++ DataStructureAndAlgorithms/src/main/java/com/ppx/algorithm/recursion_multi/E01Fibonacci.java
@@ -0,0 +1,24 @@
+package com.ppx.algorithm.recursion_multi;
+
+/**
+ * 斐波那契数列
+ */
+public class E01Fibonacci {
+
+    public static int f(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+//        int x = f(n - 1);
+//        int y = f(n - 2);
+//        return x + y;
+        return  f(n - 1)+ f(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int f = f(2);
+        System.out.println("f = " + f);
+    }
+}
--- DataStructureAndAlgorithms/src/main/java/com/ppx/algorithm/recursion_single/E05InsertionSort.java
+++ DataStructureAndAlgorithms/src/main/java/com/ppx/algorithm/recursion_single/E05InsertionSort.java
@@ -1,19 +1,50 @@
 package com.ppx.algorithm.recursion_single;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 /**
  * 插入排序
  */
 public class E05InsertionSort {
 
     public static void sort(int[] arr) {
-        insertion(arr, arr.length - 1);
+        insertion(arr, 1);
     }
 
     private static void insertion(int[] arr, int low) {
+        if (low == arr.length) {
+            return;
+        }
 
+        int t = arr[low];
+        int i = low - 1;// 已排序区域指针
+        while (i >= 0 && arr[i] > t) {
+            arr[i + 1] = arr[i];// 空出插入位置
+            i--;
+        }
+        if (i+1 != low){
+            arr[i + 1] = t;// 找到插入位置
+        }
+
+        insertion(arr, low+1);
     }
 
     public static void main(String[] args) {
+        int[] expected = {1, 2, 3, 4, 5};
+        int[] a1 = {5, 4, 3, 2, 1};
+        sort(a1);
+        assertArrayEquals(expected, a1);
 
+//        int[] a2 = {4, 5, 3, 2, 1};
+//        sort(a2);
+//        assertArrayEquals(expected, a2);
+//
+//        int[] a4 = {3, 1, 4, 2, 5};
+//        sort(a4);
+//        assertArrayEquals(expected, a4);
+//
+//        int[] a3 = {1, 2, 3, 4, 5};
+//        sort(a3);
+//        assertArrayEquals(expected, a3);
     }
 }
